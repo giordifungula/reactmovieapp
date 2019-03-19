1efa86fa09e3578480b02a1fe42546f5b191488d
giordifungula <giordifungula@gmail.com>
2019-03-19 23:17:37 +0200

    added movie list item and calling api in movie.js

--- src/main/movies/Movies.js
+++ src/main/movies/Movies.js
@@ -2,12 +2,32 @@
 
 import React from "react";
 import "./Movies.css";
+import MovieListItem from "./MovieListItem";
+
+const movies = ["Breaking bad", "Narcos", "Game of Thrones"];
 
 class Movies extends React.Component {
+  componentDidMount() {
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+
+    console.log("Before or after data?");
+  }
+
   render() {
     return (
-      <section className="movies">
-        <h1>Movies from Movies</h1>
+      <section>
+        <ul className="movies">
+          {
+            movies.map( (title, index) => (
+              <MovieListItem key={index.toString()} title={title} />
+            ))
+          }
+        </ul>
       </section>
     )
   }
